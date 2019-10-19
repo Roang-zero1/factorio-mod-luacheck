db7b016105aa64416b4897cb71840913e6f376dc
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-10-20 00:07:10 +0200

    Add backwards compatibility

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-if [ -z "$LUACHECKRC_URL" ]; then
+# Backwards compatibility mapping
+if [ -z $LUACHECKRC_URL ]; then :; else
+  INPUT_LUACHECKRC_URL=$LUACHECKRC_URL
+fi
+
+if [ -z "$INPUT_LUACHECKRC_URL" ]; then
   if [ ! -f .luacheckrc ]; then
-    echo 'ERROR: No .luacheckrc present and no $LUACHECKRC_URL provided' 1>&2
+    echo 'ERROR: No .luacheckrc present and no $INPUT_LUACHECKRC_URL provided' 1>&2
     exit 1
   fi
 else
-  echo "Downloading luacheckrc file from ${LUACHECKRC_URL}"
+  echo "Downloading luacheckrc file from ${INPUT_LUACHECKRC_URL}"
   HTTP_RESPONSE=$(curl --write-out "HTTPSTATUS:%{http_code}" \
     -sSL \
     -o .luacheckrc \
-    $LUACHECKRC_URL)
+    $INPUT_LUACHECKRC_URL)
 
   HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
   if [ $HTTP_STATUS -ne 200 ]; then
